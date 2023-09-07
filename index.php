a92f28963cc616b15025261d112a8c5740984915
mich1 <Mich.1999@hotmail.com>
2023-09-07 09:19:11 +0200

    nav og font awesome added

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	
-	<title>Sigende titel</title>
+	<title>Portfolio</title>
 	
 	<meta name="robots" content="All">
 	<meta name="author" content="Udgiver">
@@ -16,7 +16,33 @@
 </head>
 
 <body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary  gy-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">
+            portfolio
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active d-lg-none" aria-current="page" href="#">Home</a>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#skoleopgaver">skole projekter</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#fritid">fritidsprojekter</a>
+                </li>
 
+                <li class="nav-item">
+                    <a class="nav-link active" href="#om">om mig</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
