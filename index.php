33d2a792500653d6fcd9e7c01d632cc37234e4a5
mich1 <Mich.1999@hotmail.com>
2023-09-07 12:33:20 +0200

    side stylet og lavet

--- index.php
+++ index.php
@@ -11,23 +11,26 @@
 	
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.4.2/css/all.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Roboto:wght@100;300&display=swap" rel="stylesheet">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary  gy-3">
+<nav class="navbar navbar-expand-lg ">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
             portfolio
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
+
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active d-lg-none" aria-current="page" href="#">Home</a>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul class="navbar-nav mb-auto align-items-end">
+
                 <li class="nav-item">
                     <a class="nav-link active" href="#skoleopgaver">skole projekter</a>
                 </li>
@@ -55,7 +58,7 @@
             <div class="card col-12 col-md-6 col-lg-4">
                 <img src="images/foto%20film%20fællesskab%20logo.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title text-uppercase">b2b foto og videoeksamensopgave</h5>
+                    <h5 class="card-title text-uppercase">p2p foto og videoeksamensopgave</h5>
                     <p class="card-text"> denne opgave gik ud på at men skulle opsætte en hjemmeside for en b2b virksomhed.</p>
                     <a href="http://multimediemichael.dk/eksamen%202/index.html" class="btn btn-primary"> hjemmesiden jeg lavet</a>
 
@@ -63,10 +66,11 @@
             </div>
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="card">
-                    <img src="images/playstation%20mønster-05.png" class="card-img-top" alt="mønsteropgave">
+                    <img src="images/vilma_logo.png" class="card-img-top" alt="vilmalogo">
                     <div class="card-body">
-                        <h5 class="card-title  text-uppercase">mønster opgave</h5>
-                        <p class="card-text">denne opgave gik ud på at lave et mønster ud fra et brand og med det valgte jeg playstation som brand og med inspiration fra memphis design stilen og playstations symboer kom jeg frem til dette</p>
+                        <h5 class="card-title  text-uppercase">filmfetival</h5>
+                        <p class="card-text">denne opgave gik ud på at lave en app til en fiktiv filmfestival hvor man kunne se hvad der spillede af film og hvilke gæster der kom på de valgte dage jeg har været en del af kodningen til appen</p>
+                        <a href="http://multimediemichael.dk/temaopgave/index.php" class="btn btn-primary"> appen</a>
 
                     </div>
                 </div>
@@ -104,7 +108,7 @@
                 <div class="card">
                     <img src="images/space%20test-01.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">rum og rumvæsen illustration</h5>
+                        <h5 class="card-title text-uppercase">rum og rumvæsen illustration</h5>
                         <p class="card-text">efter en plakatopgave der blev lavet på skolen blev fik jeg lyst til at lave en illustration af et rumvæsen i flyvende tallerken i rummet .</p>
 
                     </div>
@@ -114,7 +118,7 @@
                 <div class="card">
                     <img src="images/skull-01.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">skull</h5>
+                        <h5 class="card-title text-uppercase">skull</h5>
                         <p class="card-text">jeg blev inspireret af et  show ved navn Bleach til at prøve at lave det her stylized kranium i illustrator .</p>
 
                     </div>
@@ -124,8 +128,8 @@
                 <div class="card">
                     <img src="images/turtle%20shell-01.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">skildpaddeskjold</h5>
-                        <p class="card-text">.</p>
+                        <h5 class="card-title text-uppercase">skildpaddeskjold</h5>
+                        <p class="card-text">jeg lavet dette skildpadde skjold fordi jeg blev inspireret af en af mine barndons yndlingserier ninja turtles.</p>
                     </div>
                 </div>
             </div>
@@ -136,19 +140,19 @@
 </section>
 <!-- om mig card-->
 <section id="om">
-    <div class=" container">
+    <div class=" container ">
         <div class="row justify-content-center">
-            <div class="card m-3">
+            <div class="card om m-3">
                 <div class="row g-0">
                     <div class="col-md-4">
-                        <img src="images/michael.png" class="img-fluid rounded-circle rounded-start" alt="michael billede">
+                        <img src="images/michael.png" class="img-fluid rounded-circle start" alt="michael billede">
                     </div>
                     <div class=" col-sm-12 col-md-8 ">
                         <div class="card-body">
                             <h5 class="card-title">ophavsret</h5>
-                            <p class="card-text"> alt hvad der vises her er originalt og tilhører mig</p>
+                            <p class="card-text"> alt hvad der vises her er originalt.</p>
                             <h5 class="card-title">Om Mig</h5>
-                            <p class="card-text"> Jeg hedder Michael, jeg er 24 år gammel, jeg har cerebral parese hvliket betyder jeg spastisk lammet i min ene arm og i mine ben.mine ambitioner for fremtiden er enten frontend design eller grafisk design. jeg er specialiseret i avanceret digital udvikling </p>
+                            <p class="card-text"> Jeg hedder Michael, jeg er 24 år gammel, jeg har cerebral parese, hvliket betyder jeg er spastisk lammet i min højre arm og i mine ben. jeg valgte uddannelsen som multimediedesigner fordi jeg synes det lød spændende at lærer hvordan man designer og sætter en hjemmeside op.mine ambitioner for fremtiden er enten frontend design eller grafisk design. jeg er specialiseret i avanceret digital udvikling </p>
                             <h5 class="card-title"> færdigheder</h5>
                             <p><ul>
                                 <li>bootstrap</li>
@@ -176,6 +180,23 @@
 
 </section>
 
+<footer>
+    <div class="container-fluid text-dark text-center bg-dark-subtle">
+
+        <p class="display-4 mb-1 text-uppercase">socialmedier</p>
+        <a class="btn btn-outline-success btn-floating mb-2" href="https://www.facebook.com/profile.php?id=100000312770132" role="button">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+
+        <a class="btn btn-outline-success btn-floating mb-2" href="https://www.linkedin.com/in/michael-mørkeberg/" role="button"
+        ><i class="fab fa-linkedin-in"></i
+            ></a>
+        <a class="btn btn-outline-success btn-floating mb-2" href="https://github.com/Mick3423" role="button"
+        ><i class="fab fa-github"></i
+            ></a>
+
+    </div>
+</footer>
 
 
 
