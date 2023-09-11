4f1b26d98008ce701ba72cb47df86cee50df4a70
mich1 <Mich.1999@hotmail.com>
2023-09-11 12:08:04 +0200

    svamp card added

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg ">
+<nav class="navbar navbar-expand-lg bg-dark-subtle ">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
             portfolio
@@ -46,6 +46,24 @@
         </div>
     </div>
 </nav>
+<!--hero-->
+
+<div class="container pb-5 my-5 bg-dark-subtle">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+        <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+            <h3 class="display-4 fw-bold lh-1">multimediedesigner med en kreativ sans for detaljer</h3>
+            <p class="lead"> jeg hedder Michael. jeg er en ung fyr, der er klar på nye udfordringer og at få en hel masse,viden og samtidig blive en hel del klogere på design</p>
+
+        </div>
+        <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden ">
+            <img class="rounded-circle" src="images/michael.png" alt="michael foto" >
+        </div>
+    </div>
+</div>
+
+
+<br>
+<br>
 
 <!--skoleopgaver -->
 <section  id="skoleopgaver">
@@ -56,10 +74,10 @@
             </div>
 
             <div class="card col-12 col-md-6 col-lg-4">
-                <img src="images/foto%20film%20fællesskab%20logo.png" class="card-img-top" alt="...">
+                <img src="images/foto%20film%20fællesskab%20logo.png" class="card-img-top" alt="fotofilmhjemmeside">
                 <div class="card-body">
-                    <h5 class="card-title text-uppercase">p2p foto og film eksamenside</h5>
-                    <p class="card-text"> Denne opgave gik ud på at men skulle opsætte en hjemmeside for udlån af udstyr en fiktiv p2p virksomhed.</p>
+                    <h5 class="card-title text-uppercase"> foto og film eksamenside</h5>
+                    <p class="card-text"> Denne opgave gik ud på at men skulle opsætte en hjemmeside for udlån af udstyr en fiktiv peer-to-peer virksomhed.</p>
                     <a href="http://multimediemichael.dk/eksamen%202/index.html" class="btn btn-primary"> hjemmesiden jeg lavet</a>
 
                 </div>
@@ -69,7 +87,7 @@
                     <img src="images/vilma_logo.png" class="card-img-top" alt="vilmalogo">
                     <div class="card-body">
                         <h5 class="card-title  text-uppercase">filmfetival</h5>
-                        <p class="card-text">denne opgave gik ud på at lave en app til en fiktiv filmfestival. Hvor man kunne se hvad der spillede af film, og hvilke gæster der kom på de valgte dage. jeg har været en del af kodningen til appen</p>
+                        <p class="card-text">Denne opgave gik ud på at lave en app til en fiktiv filmfestival. Hvor man kunne se hvad der spillede af film, og hvilke gæster der kom på de valgte dage. jeg har været en del af kodningen til appen og designet også logoet</p>
                         <a href="http://multimediemichael.dk/temaopgave/index.php" class="btn btn-primary"> appen</a>
 
                     </div>
@@ -83,12 +101,21 @@
                     <img src="images/spjæt%20maskot-01.png" class="card-img-top" alt="maskot"">
                     <div class="card-body">
                         <h5 class="card-title  text-uppercase">børnetøjbutik maskot</h5>
-                        <p class="card-text">denne opgave handlede om at lave en maskot til en børnetøjbutik </p>
+                        <p class="card-text">Denne opgave handlede om at lave en maskot til en børnetøjbutik.  </p>
 
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="col-12 col-md-6 col-lg-4 ">
+                <div class="card ">
+                    <img src="images/svamplogo.png" class="card-img-top" alt="svamp"">
+                    <div class="card-body">
+                        <h5 class="card-title  text-uppercase">svamp</h5>
+                        <p class="card-text">Denne opgave gik ud på at man skulle lavet et forslag til en app om svampe i XD  </p>
+                        <a href="https://xd.adobe.com/view/d9583a2d-1004-40f0-804e-683546041512-8c0b/?fullscreen">app forslag</a>
+
+                    </div>
+
 
 
     </div>
@@ -109,7 +136,7 @@
                     <img src="images/space%20test-01.png" class="card-img-top" alt="rumvæsen">
                     <div class="card-body">
                         <h5 class="card-title text-uppercase">rum og rumvæsen illustration</h5>
-                        <p class="card-text">efter en plakatopgave der blev lavet på skolen, fik jeg lyst til at lave en illustration, af et rumvæsen i en flyvende tallerken i rummet .</p>
+                        <p class="card-text">Efter en plakatopgave der blev lavet på skolen, fik jeg lyst til at lave en illustration, af et rumvæsen i en flyvende tallerken i rummet.</p>
 
                     </div>
                 </div>
@@ -119,7 +146,7 @@
                     <img src="images/skull-01.png" class="card-img-top" alt="skull">
                     <div class="card-body">
                         <h5 class="card-title text-uppercase">skull</h5>
-                        <p class="card-text">jeg blev inspireret af et  show ved navn Bleach til at prøve at lave, det her stylized kranium i illustrator .</p>
+                        <p class="card-text">Jeg blev inspireret af et show ved navn Bleach til at prøve at lave, det her stylized kranium i illustrator .</p>
 
                     </div>
                 </div>
@@ -128,11 +155,12 @@
                 <div class="card">
                     <img src="images/turtle%20shell-01.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title text-uppercase">skildpadde skjold</h5>
-                        <p class="card-text">jeg lavet dette skildpadde skjold fordi jeg blev inspireret af en af mine barndoms yndlingserier ninja turtles.</p>
+                        <h5 class="card-title text-uppercase">Skildpadde skjold</h5>
+                        <p class="card-text">Jeg lavet dette skildpadde skjold fordi jeg blev inspireret af en af mine barndoms yndlingserier ninja turtles.</p>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 
@@ -143,17 +171,13 @@
     <div class=" container ">
         <div class="row justify-content-center">
             <div class="card om m-3">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="images/michael.png" class="img-fluid rounded-circle start" alt="michael billede">
-                    </div>
+
                     <div class=" col-sm-12 col-md-8 ">
                         <div class="card-body">
-                            <h5 class="card-title">ophavsret</h5>
-                            <p class="card-text"> alt hvad der vises her er originalt.</p>
                             <h5 class="card-title">Om Mig</h5>
-                            <p class="card-text"> Jeg hedder Michael, jeg er 24 år gammel og jeg har cerebral parese, hvliket betyder jeg er spastisk lammet i min højre arm og i mine ben. jeg valgte uddannelsen som multimediedesigner fordi jeg synes det lød spændende at lærer hvordan man designer og sætter en hjemmeside op.mine ambitioner for fremtiden er enten frontend design eller grafisk design. jeg er ved at blive specialiseret i avanceret digital udvikling </p>
+                            <p class="card-text">    jeg har cerebral parese, hvilket betyder jeg er spastisk lammet i min højre arm og i mine ben. Jeg valgte uddannelsen som multimediedesigner, fordi jeg synes det lød spændende at lærer hvordan man designer og sætter en hjemmeside op. Jeg er ved at blive specialiseret i avanceret digital udvikling </p>
                             <h5 class="card-title"> færdigheder</h5>
+                            <p>det her de ting jeg har færdigheder indefor </p>
                             <p><ul>
                                 <li>bootstrap</li>
                                 <li>html</li>
@@ -161,6 +185,8 @@
                                 <li>illustrator</li>
                                 <li>photoshop</li>
                                 <li>premier pro</li>
+                                <li>XD</li>
+                                <li>sociale medie posts</li>
 
                             </ul>
                             <h5>Kontaktoplysninger
@@ -176,14 +202,14 @@
             </div>
         </div>
 
-    </div>
+
 
 </section>
 
 <footer>
     <div class="container-fluid text-dark text-center bg-dark-subtle">
 
-        <p class="display-4 mb-1 text-uppercase">socialmedier</p>
+        <p class="display-4 mb-1 text-uppercase">socialemedier</p>
         <a class="btn btn-outline-success btn-floating mb-2" href="https://www.facebook.com/profile.php?id=100000312770132" role="button">
             <i class="fab fa-facebook-f"></i>
         </a>
@@ -194,7 +220,8 @@
         <a class="btn btn-outline-success btn-floating mb-2" href="https://github.com/Mick3423" role="button"
         ><i class="fab fa-github"></i
             ></a>
-
+        <br>
+<span>  &copy;alt hvad der ses her er originalt </span>
     </div>
 </footer>
 
